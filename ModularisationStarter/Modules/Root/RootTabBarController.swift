90689e23473f73b0b0dd3bf6844771b9ce37f054
Akadet Mathurapoj <akadet.m@lmwn.com>
2024-04-01 21:44:00 +0700

    move Utilities module

--- ModularisationStarter/Modules/Root/RootTabBarController.swift
+++ ModularisationStarter/Modules/Root/RootTabBarController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Utilities
 
 final class RootTabBarController: UITabBarController {
 
--- Packages/Utilities/Sources/Utilities/String+localised.swift
+++ Packages/Utilities/Sources/Utilities/String+localised.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-extension String {
+public extension String {
     
     var localised: String {
         NSLocalizedString(self, comment: "")
